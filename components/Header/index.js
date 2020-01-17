18631345aca00404db06ec3bd7a264b769552c19
Daniel Fieker <danielmfieker@gmail.com>
2020-01-17 11:26:20 -0600

    Creating Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,37 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+// Create Elements
+
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+// Set Up Element Structure
+
+    header.append(date);
+    header.append(title);
+    header.append(temp);
+
+// Add Classes to Elements
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+// Set Text Content
+
+    date.textContent = "SMARCH 28, 2019";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+return header;
+}
+
+// 
+const newHeader = document.querySelector('.header-container');
+console.log(newHeader);
+newHeader.append(Header);
